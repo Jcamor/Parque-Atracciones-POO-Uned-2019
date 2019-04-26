95a0f0909c32d53dbaaf581b4acc33777e862fc3
Juan Carlos <jcamorgutierrez@gmail.com>
2019-04-26 21:12:06 +0200

    introducir clase empleado

---
+++ Empleado.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class Empleado here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Empleado
+{
+    // instance variables - replace the example below with your own
+    private String dni;
+    private String nombre;
+    private String puesto;
+    private int telefono;
+    private String direccion;
+    private double sueldoMes;
+
+    /**
+     * Constructor for objects of class Empleado
+     */
+    public Empleado()
+    {
+        // initialise instance variables
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    
+}
