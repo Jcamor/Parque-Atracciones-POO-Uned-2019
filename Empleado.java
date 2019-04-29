8d3546997e40d60012cbbc163409e9144dfff19e
Juan Carlos <jcamorgutierrez@gmail.com>
2019-04-29 20:01:24 +0200

    acabado empleados

--- Empleado.java
+++ Empleado.java
@@ -19,10 +19,10 @@ public class Empleado {
     private String direccion;
     private double sueldoMes;
     final double sueldoBase = 950;
-    public ArrayList<Empleado> empleados;
+   // public ArrayList<Empleado> empleados;
 
-    public Empleado(String dni, String nombre, String puesto, int telefono,
-            String direccion, double sueldoMes) {
+    public Empleado(String dni, String nombre, String direccion, int telefono,
+            String puesto, double sueldoMes) {
         this.dni = dni;
         this.nombre = nombre;
         this.puesto = puesto;
@@ -78,123 +78,4 @@ public class Empleado {
     public void setSueldoMes(double sueldoMes) {
         this.sueldoMes = sueldoMes;
     }
-
-    public void menuEmpleados() {
-        empleados = new ArrayList<Empleado>();
-        Scanner sn = new Scanner(System.in);
-        boolean salir = false;
-        int opcion; //Guardaremos la opcion del usuario
-        while (!salir) {
-            System.out.println("\n\n\n\n\n\n");
-            System.out.println("    MENU EMPLEADOS PARQUE ATRACCIONES");
-            System.out.println("    =================================");
-            System.out.println("      [1]. Añadir Empleado");
-            System.out.println("      [2]. Borrar Empleado");
-            System.out.println("      [3]. Listar Empleado");
-            System.out.println("      [4]. Salir");
-
-            try {
-
-                System.out.println("\n\n\n");
-                System.out.println("    Escribe una de las opciones del menu");
-                System.out.print("    > ");
-                opcion = sn.nextInt();
-
-                switch (opcion) {
-                    case 1:
-                        agregarEmpleado();
-                        break;
-                    case 2:
-                        //borrarEmpleado();
-                        break;
-                    case 3:
-                        //listarEmpleado();
-                        break;
-                    case 4:
-                        salir = true;
-                        break;
-                    default:
-                        System.out.println("\n\n");
-                        System.out.println("Solo números entre 1 y 4");
-                        System.out.println("\n\n");
-                }
-            } catch (InputMismatchException e) {
-
-                System.out.println("\n\n");
-                System.out.println("Solo números entre 1 y 4");
-                System.out.println("\n\n");
-                sn.next();
-            }
-        }
-    }
-
-    public void agregarEmpleado() {
-
-        Scanner sn = new Scanner(System.in);
-        int opcion; //Guardaremos la opcion del usuario
-
-        try {
-            System.out.println();
-            System.out.println();
-            System.out.println("Introducir datos Empleado");
-            System.out.println("=========================");
-            System.out.println();
-
-            System.out.print("Dni: ");
-            setDni(sn.nextLine());
-            System.out.println();
-
-            System.out.print("Nombre: ");
-            setNombre(sn.nextLine());
-            System.out.println();
-
-            System.out.print("Direccion: ");
-            setDireccion(sn.nextLine());
-            System.out.println();
-
-            System.out.print("Telefono: ");
-            setTelefono(sn.nextInt());
-            System.out.println();
-
-            System.out.println("       Puesto de trabajo");
-            System.out.println("       =================");
-            System.out.println("      [1]. Atencion al Cliente");
-            System.out.println("      [2]. Relaciones Publicas");
-            System.out.println("      [3]. Responsables de Atraccion");
-            System.out.println("      [4]. Ayudante de Atraccion");
-            System.out.println();
-            System.out.println("    Escribe una de las opciones del puesto");
-            System.out.print("    > ");
-            opcion = sn.nextInt();
-
-            switch (opcion) {
-                case 1:
-                    setPuesto("Atencion al cliente");
-                    setSueldoMes(sueldoBase * 1.10);
-                    break;
-                case 2:
-                    setPuesto("Relaciones Publicas");
-                    setSueldoMes(sueldoBase * 1.20);
-                    break;
-                case 3:
-                    setPuesto("Responsables de Atraccion");
-                    setSueldoMes(sueldoBase * 1.15);
-                    break;
-                case 4:
-                    setPuesto("Ayudante de Atraccion");
-                    setSueldoMes(sueldoBase);
-                    break;
-                default:
-                    System.out.println("\n\n");
-                    System.out.println("Solo números entre 1 y 4");
-                    System.out.println("\n\n");
-            }
-        } catch (InputMismatchException e) {
-            System.out.println("Solo numeros");
-            sn.next();
-        }
-    }
-    Empleado emple = new Empleado(dni, nombre, puesto, telefono,
-            direccion, sueldoMes);
-            empleados.add(emple);
 }
--- Menu.java
+++ Menu.java
@@ -2,7 +2,7 @@
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
-
+import java.util.Iterator;
 
 /**
  * Write a description of class Entradas here.
@@ -17,6 +17,7 @@ public class Menu {
      */
     private boolean ventaAnticipada = false;
     private boolean entradaFamilia = false;
+
     public ArrayList<Cliente> clientes;
     public ArrayList<Empleado> empleados;
 
@@ -37,7 +38,7 @@ public class Menu {
             System.out.println("    ==========================");
             System.out.println("      [1]. Menu Entradas");
             System.out.println("      [2]. Menu Empleados");
-            System.out.println("      [3]. Menu Datos Estadísticos");
+            System.out.println("      [3]. Menu Datos EstadÃ­sticos");
             System.out.println("      [4]. Salir");
 
             try {
@@ -52,7 +53,9 @@ public class Menu {
                         menuEntradasPrincipal();
                         break;
                     case 2:
-                        empleado.menuEmpleados();                        
+                        //Empleado e = new Empleado();
+                        //ArrayList<Empleado> empleados = new ArrayList<Empleado>();
+                        menuEmpleados();
                         break;
                     case 3:
                         System.out.println("Has seleccionado la opcion 3");
@@ -63,13 +66,13 @@ public class Menu {
                     default:
                         limpiarPantalla();
                         System.out.println("\n\n");
-                        System.out.println("Solo números entre 1 y 4");
+                        System.out.println("Solo nÃºmeros entre 1 y 4");
                     //System.out.println("\n\n");
                 }
             } catch (InputMismatchException e) {
                 limpiarPantalla();
                 System.out.println("\n\n");
-                System.out.println("Solo números entre 1 y 4");
+                System.out.println("Solo nÃºmeros entre 1 y 4");
                 //System.out.println("\n\n");
                 sn.next();
             }
@@ -179,5 +182,226 @@ public class Menu {
 
     public void limpiarPantalla() {
         System.out.println("\n\n\n\n\n\n");
+
+    }
+
+    public void menuEmpleados() {
+        Scanner sn = new Scanner(System.in);
+        boolean salir = false;
+        int opcion;
+        while (!salir) {
+            System.out.println("\n\n\n\n\n\n");
+            System.out.println("    MENU EMPLEADOS PARQUE ATRACCIONES");
+            System.out.println("    =================================");
+            System.out.println("      [1]. Añadir Empleado");
+            System.out.println("      [2]. Borrar Empleado");
+            System.out.println("      [3]. Listar Empleado");
+            System.out.println("      [4]. Salir");
+
+            try {
+
+                System.out.println("\n\n\n");
+                System.out.println("    Escribe una de las opciones del menu");
+                System.out.print("    > ");
+                opcion = sn.nextInt();
+
+                switch (opcion) {
+                    case 1:
+                        agregarEmpleado();
+                        break;
+                    case 2:
+                        borrarEmpleado();
+                        break;
+                    case 3:
+                        listarEmpleados();
+                        break;
+                    case 4:
+                        salir = true;
+                        break;
+                    default:
+                        System.out.println("\n\n");
+                        System.out.println("Solo números entre 1 y 4");
+                        System.out.println("\n\n");
+                }
+            } catch (InputMismatchException e) {
+
+                System.out.println("\n\n");
+                System.out.println("Solo números entre 1 y 4");
+                System.out.println("\n\n");
+                sn.next();
+            }
+        }
+    }
+
+    public void agregarEmpleado() {
+
+        Scanner sn = new Scanner(System.in);
+        int opcion; //Guardaremos la opcion del usuario
+        String dni = "";
+        String nombre = "";
+        String puesto = "";
+        int telefono = 0;
+        String direccion = "";
+        double sueldoMes = 0;
+        double sueldoBase = 950;
+
+        System.out.println();
+        System.out.println();
+        System.out.println("Introducir datos Empleado");
+        System.out.println("=========================");
+        System.out.println();
+
+        System.out.print("Dni: ");
+        dni = sn.nextLine();
+        System.out.println();
+
+        System.out.print("Nombre: ");
+        nombre = sn.nextLine();
+        System.out.println();
+
+        System.out.print("Direccion: ");
+        direccion = sn.nextLine();
+        System.out.println();
+        boolean salir = false;
+        while (!salir) {
+            try {
+                System.out.print("Telefono: ");
+                telefono = sn.nextInt();
+                System.out.println();
+                salir = true;
+            } catch (InputMismatchException e) {
+
+                System.out.println("\n\n");
+                System.out.println("Solo números para el telefono");
+                System.out.println("\n\n");
+                sn.next();
+            }
+        }
+        opcion = 0;
+        salir=false;
+        while (!salir) {
+            try {
+                System.out.println("       Puesto de trabajo");
+                System.out.println("       =================");
+                System.out.println("      [1]. Atencion al Cliente");
+                System.out.println("      [2]. Relaciones Publicas");
+                System.out.println("      [3]. Responsables de Atraccion");
+                System.out.println("      [4]. Ayudante de Atraccion");
+                System.out.println();
+                System.out.println("    Escribe una de las opciones del puesto");
+                System.out.print("    > ");
+                opcion = sn.nextInt();
+
+                switch (opcion) {
+                    case 1:
+                        puesto = "Atencion al cliente";
+                        sueldoMes = sueldoBase * 1.10;
+                        salir = true;
+                        break;
+                    case 2:
+                        puesto = "Relaciones Publicas";
+                        sueldoMes = sueldoBase * 1.20;
+                        salir = true;
+                        break;
+                    case 3:
+                        puesto = "Responsables de Atraccion";
+                        sueldoMes = sueldoBase * 1.15;
+                        salir = true;
+                        break;
+                    case 4:
+                        puesto = "Ayudante de Atraccion";
+                        sueldoMes = sueldoBase;
+                        salir = true;
+                        break;
+                    default:
+                        System.out.println("\n\n");
+                        System.out.println("Solo números entre 1 y 4");
+                        System.out.println("\n\n");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Solo numeros");
+                sn.next();
+
+            }
+        }
+        Empleado emple = new Empleado(dni, nombre, direccion, telefono, puesto, sueldoMes);
+        System.out.println(emple.getDni() + " " + emple.getNombre() + " " + emple.getDireccion()
+                + " " + emple.getTelefono() + " " + emple.getPuesto() + " " + emple.getSueldoMes());
+        if (empleados.add(emple)) {
+            System.out.println();
+            System.out.println("El empleado " + emple.getNombre() + " ha sido grabado correctamente");
+        } else {
+            System.out.println();
+            System.out.println("El empleado " + emple.getNombre() + " NO ha sido grabado");
+        }
+    }
+
+    public void borrarEmpleado() {
+        Scanner sn = new Scanner(System.in);
+        String dniBorrar = "";
+        boolean confirmarBorrar = false;
+        System.out.println();
+        System.out.print("Introduce DNI del empleado a eliminar: ");
+        dniBorrar = sn.nextLine();
+        Iterator<Empleado> it = empleados.iterator();
+        while (it.hasNext()) {
+            Empleado emplea = it.next();
+            if (dniBorrar.equalsIgnoreCase(emplea.getDni())) {
+                System.out.println();
+                System.out.println("Dni: " + emplea.getDni() + " Nombre: " + emplea.getNombre());
+                confirmarBorrar = preguntasVarias("Deseas borrar este empleado");
+                if (confirmarBorrar) {
+                    System.out.println();
+                    System.out.println("El empleado " + emplea.getNombre() + " ha sido borrado");
+                    empleados.remove(emplea);
+                } else {
+                    System.out.println();
+                    System.out.println("El empleado " + emplea.getNombre() + " NO ha sido borrado");
+                }
+            } else {
+                System.out.println("DNI no encontrado");
+            }
+        }
+    }
+
+    public void listarEmpleados() {
+        boolean salir = false;
+        for (Empleado emplea : empleados) {
+            System.out.println();
+            System.out.println("Dni: " + emplea.getDni());
+            System.out.println("Nombre: " + emplea.getNombre());
+            System.out.println("Direccion: " + emplea.getDireccion());
+            System.out.println("Telefono: " + emplea.getTelefono());
+            System.out.println("Puesto: " + emplea.getPuesto());
+            System.out.println("Sueldo Mes: " + emplea.getSueldoMes());
+            System.out.println();
+
+        }
+    }
+
+    public boolean preguntasVarias(String pregunta) {
+        Scanner sn = new Scanner(System.in);
+        boolean salir = false;
+        String respuesta = "";
+        boolean respuestaDevuelta = false;
+
+        try {
+            do {
+                System.out.print(pregunta + " (S/N) :");
+                respuesta = sn.nextLine();
+                if (respuesta.equalsIgnoreCase("s")) {
+                    respuestaDevuelta = true;
+                    salir = true;
+                }
+                if (respuesta.equalsIgnoreCase("n")) {
+                    respuestaDevuelta = false;
+                    salir = true;
+                }
+                System.out.println();
+            } while (!salir);
+        } catch (InputMismatchException e) {
+
+        }
+        return respuestaDevuelta;
     }
 }
